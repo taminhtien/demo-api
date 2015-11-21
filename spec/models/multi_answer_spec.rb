41998280601844660226c68eea51c1b9c8df4dad
Tien Ta <taminhtien1993@gmail.com>
2015-11-21 14:31:22 +0700

    Fix typo

--- spec/models/multi_answer_spec.rb
+++ spec/models/multi_answer_spec.rb
@@ -15,9 +15,9 @@ RSpec.describe MultiAnswer, type: :model do
     let!(:python)         { create(:choice, name: 'Python', question: question) }
     let!(:ruby_answers)   { create_list(:multi_answer, 3, question: question, choice_id: ruby.id) }
     let!(:python_answers) { create_list(:multi_answer, 2, question: question, choice_id: python.id) }
-    let!(:expected)       { [ {ruby.id => 0.6}, { python.id => 0.4}] }
+    let!(:expected)       { [{ ruby.id => 0.6 }, { python.id => 0.4 }] }
     
-    it "returns an array of ruby_answers" do
+    it "returns an desirable json containing percentages of each choices" do
       expect(MultiAnswer.result(question.id)).to eq expected
     end
   end
