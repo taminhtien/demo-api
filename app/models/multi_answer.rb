e7f40f4426f99ed4caab552f5c90375aecf948de
Tien Ta <taminhtien1993@gmail.com>
2015-11-21 13:43:20 +0700

    Remove reverse in MultiAnswer.result

--- app/models/multi_answer.rb
+++ app/models/multi_answer.rb
@@ -3,7 +3,7 @@ class MultiAnswer < Answer
   validates :choice_id, presence: true
 
   def self.result(question_id)
-    answers = where(question_id: question_id)    
+    answers = where(question_id: question_id)
     groups = answers.group('choice_id').count
     total = answers.count
 
@@ -13,6 +13,6 @@ class MultiAnswer < Answer
       percent = no.to_f / total.to_f if total > 0
       results << { choice_id => percent }
     end
-    results.reverse
+    results
   end
 end
